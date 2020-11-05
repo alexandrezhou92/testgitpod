5dcc9e98f2eab5aac5c0590abf540712d227d3d0
alexandrezhou92 <alexandre.zhou@icloud.com>
2020-11-05 15:04:03 +0000

    new commit

--- hello.py
+++ hello.py
@@ -1,7 +1,2 @@
-import pandas as pd  
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-ser = pd.Series([0, 1, 2, 3, 4, 5, 6])
-ser.cumsum()
-plt.show()
+if __name__ == '__main__':
+    print('hello')
